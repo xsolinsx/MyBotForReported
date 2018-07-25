78d986a2b18466eb1cabe33aee350b4d3bf6ebfd
xsolinsx <ericsolinas@hotmail.it>
2018-07-25 03:50:31 +0200

    fix

--- bot.lua
+++ bot.lua
@@ -567,13 +567,13 @@ function on_msg_receive(msg)
                     forwardMessage(user.id, msg.from.id, msg.message_id)
                     if msg.media then
                         if msg.media_type == 'sticker' then
-                            sendMessage(user.id, '↑ STICKER ' .. msg.from.print_name .. ' (' .. msg.from.id .. ') ↑')
+                            sendMessage(user.id, '↑ STICKER ' .. msg.from.print_name .. ' (#user' .. msg.from.id .. ') ↑')
                         end
                     end
                     if msg.forward then
                         if msg.forward_from then
                             if msg.forward_from.id ~= msg.from.id then
-                                sendMessage(user.id, '↑ FWD USER ' .. msg.from.print_name .. ' (' .. msg.from.id .. ') ↑')
+                                sendMessage(user.id, '↑ FWD USER ' .. msg.from.print_name .. ' (#user' .. msg.from.id .. ') ↑')
                             end
                         elseif msg.forward_from_chat then
                             if msg.forward_from_chat.id ~= msg.from.id then
